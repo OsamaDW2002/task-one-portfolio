bc7d410d5a7ba88d8eeeef8e3f323dda52cbd9c2
OsamaDW <s12028609@stu.najah.edu>
2025-02-14 20:07:14 +0200

    change icons and cards images path

--- assets/data/repos.js
+++ assets/data/repos.js
@@ -2,32 +2,32 @@ export const repos = [
     {
         title: 'الاكادميه العربيه للبرمجه',
         description: "Project designed for arab code academy company, using React/NextJS and Chakra UI ",
-        image:"../../images/repos/arab-code-academy.jpg",
+        image:"images/repos/arab-code-academy.jpg",
         link:"https://github.com/OsamaDW2002/Arabcodeacademy"
     },
     {
         title: 'ball-e-controller',
         description:  "It's a web application that controls a robotic system called Ball-E, which searches for and grips balls.",
-        image:"../../images/repos/github-svgrepo-com.png",
+        image:"images/repos/github-svgrepo-com.png",
         link:""
     },
     {
         title: 'eco-track',
         description: "An API designed in javascript for environmental data sharing and collection with NLP capabilities for data classification and search.",
-        image:"../../images/repos/github-svgrepo-com.png",
+        image:"images/repos/github-svgrepo-com.png",
         link:"https://github.com/OsamaDW2002/eco_track"
     },
     {
         title: 'Stop Watch',
         description: "A ReactJS application that provides a stopwatch-based game with multiple levels, where players must stop the watch within a specific time.",
-        image:"../../images/repos/github-svgrepo-com.png",
+        image:"images/repos/github-svgrepo-com.png",
         link:"https://github.com/OsamaDW2002/react-stop-watch-game"
     },
     {
 
         title: 'anchor',
         description: "An announcement system that allows users to announce events and add prizes for participants",
-        image:"../../images/repos/github-svgrepo-com.png",
+        image:"images/repos/github-svgrepo-com.png",
         link:"https://github.com/OsamaDW2002/Anchor"
     },
 
